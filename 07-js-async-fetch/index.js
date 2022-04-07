44a5d03f655df2505a613ab0c6ef4f529ab07802
Darrian Bagley <dbagle14@gmail.com>
2022-04-07 18:11:48 -0400

    Completed Lesson 7

--- 07-js-async-fetch/index.js
+++ 07-js-async-fetch/index.js
@@ -15,35 +15,35 @@ function addPoke(e) {
     likes: 0,
   };
 
-  renderPokemon(newPoke)
+  renderPokemon(newPoke);
   // add new character object to pokemonDB. Note: this will not persist on page refresh.
-  pokemonDB.push(newPoke)
-  pokeForm.reset()
-  alert("nice job! your new poke is added to page")
+  pokemonDB.push(newPoke);
+  pokeForm.reset();
+  alert("nice job! your new poke is added to page");
 }
-
-const pokeContainer = document.querySelector("#poke-container");
-
-
-pokemonDB.forEach((obj) => renderPokemon(obj));
 
 function renderPokemon(character) {
   const pokeCard = document.createElement("div");
   pokeCard.id = `poke-${character.id}`;
   pokeCard.className = "poke-card";
+  pokeCard.addEventListener('click', () => {
+    showCharacter(character);
+  });
 
   const pokeImg = document.createElement("img");
   pokeImg.src = character.img;
   pokeImg.alt = `picture of ${character.name}`;
 
   const pokeName = document.createElement("h3");
+  pokeName.classList.add("poke-name");
   pokeName.innerText = character.name;
 
   const pokeLikes = document.createElement("h3");
+  pokeLikes.classList.add("poke-likes");
   pokeLikes.innerText = "Likes: ";
 
   const likesNum = document.createElement("h5");
-  likesNum.className = "likes-num";
+  likesNum.classList.add("likes-num");
   likesNum.textContent = character.likes;
 
   const likeBtn = document.createElement("button");
@@ -53,11 +53,10 @@ function renderPokemon(character) {
   likeBtn.addEventListener("click", addLikes);
 
   function addLikes() {
-    character.likes += 1; 
+    character.likes += 1;
     likesNum.textContent = character.likes;
   }
 
-  
   const deleteBtn = document.createElement("button");
   deleteBtn.className = "delete-btn";
   deleteBtn.textContent = "Delete";
@@ -67,4 +66,17 @@ function renderPokemon(character) {
   pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likeBtn, deleteBtn);
   pokeContainer.append(pokeCard);
 
+}
+
+const pokeContainer = document.querySelector("#poke-container");
+
+async function getPokemon() {
+  return fetch('http://localhost:3000/characters')
+    .then(response => response.json());
+}
+
+function showCharacter(character) {
+  console.log(character);
+  pokeContainer.innerHTML = "";
+  renderPokemon(character);
 }
