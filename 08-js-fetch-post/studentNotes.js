c84e06463d7c48fe22b4a2ff4c2ea5a2f4f7736b
Darrian Bagley <dbagle14@gmail.com>
2022-04-26 19:40:57 -0400

    studentNotes edited

--- 08-js-fetch-post/studentNotes.js
+++ 08-js-fetch-post/studentNotes.js
@@ -1,16 +1,31 @@
-// GLOBAL VARS
-const pokeContainer = document.querySelector("#poke-container");
-const pokeForm = document.getElementById("poke-form");
-const pokeFormContainer = document.getElementById("poke-form-container");
-const commentsContainer = document.getElementById("comments-list");
+const pokemonDB = new PokemonDB();
+pokemonDB.getAllPokemon().then(pokemonArray => {
+  console.log("Get All Pokemon:", pokemonArray);
+  renderPokemon(pokemonArray);
+})
+  .catch(err => console.log(err));
 
-// EVENT LISTENER
+// GLOBAL VARS
+const commentsContainer = document.querySelector("#comments-list");
+const pokeContainer = document.querySelector("#poke-container");
+
+
+// INIT FORM
+const pokeForm = document.createElement('form');
+pokeForm.setAttribute('id', '#poke-form');
+const pokeFormContainer = document.querySelector('#poke-form-container');
+pokeFormContainer.appendChild(pokeForm);
+
+// FORM EVENT LISTENER
 pokeForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let nameInput = e.target.children['name-input'].value;
   addPoke(nameInput);
 });
 
+
+// INVOKE INITIALIZE
+fetchAllPokemon().then(arr => renderAllPokemon(arr));
 
 // create poke function will not work with our API -- yet. But it still works with our static pokemonDB.
 function addPoke(name) {
@@ -21,7 +36,7 @@ function addPoke(name) {
 
   renderPokemon([newPoke]);
 
-  // add new character object to pokemonDB array. Note: this will not persist on page refresh.
+  // add new pokemon object to pokemonDB array. Note: this will not persist on page refresh.
   pokemonDB.push(newPoke);
 
   // clear the form
@@ -59,74 +74,76 @@ function addPokemonToDatabase(poke) {
     });
 }
 
-//  SHOW PAGE - 1 POKE
-function showCharacter(character) {
+// SHOW PAGE - 1 POKE
+function pokemonShowPage(pokemon) {
   hideAllPokemon();
+  getPokemonById(pokemon.id).then(returnedPokemon => {
+    renderPokemon([returnedPokemon]);
+    // pokeCardEl.append(commentsForm(returnedPokemon));
+    // pokeCardEl.append(renderComments(returnedPokemon));
+  });
 
-  fetch(`http://localhost:3000/characters/${character.id}`)
-    .then(resp => resp.json())
-    .then(returnedChar => {
-      const newPokeCard = renderPokemon([returnedChar]);
-      newPokeCard.id = 'poke-show-card';
-      // newPokeCard.dataset.id = returnedChar.id
-      loadComments(newPokeCard, returnedChar);
-      pokeContainer.appendChild(newPokeCard);
-    });
-  pokeFormContainer.replaceChildren(commentsForm());
+
+
+  // pokeFormContainer.replaceChildren(commentsForm(pokemon.id));
 }
 
 // TODO: add comments to show page ✏
-function loadComments(pokeCard, character) {
+function loadComments(pokeCardEl, pokemon) {
   const commentsList = document.createElement("ul");
-  commentsList.id = `comment-card-${character.id}`;
+  commentsList.id = `comment-list-${pokemon.id}`;
 
   const numComments = document.createElement("h4");
   commentsList.append(numComments);
-  pokeCard.append(commentsList);
+  pokeCardEl.append(commentsList);
 
-  const comments = character.comments;
+  const comments = pokemon.comments;
   numComments.textContent = comments.length;
   numComments.textContent += comments.length === 1 ? " comment" : " comments";
 
   comments.forEach(comment => renderComment(commentsList, comment));
 }
 
+
 // TODO: Finish function to submit comments
-function addComment(e) {
-  console.log(e);
-  const inputText = e.target.children['comment-input'].value;
+function submitComment(commentText, id) {
   const newComment = {
-    content: inputText,
-    id: comments.length + 1,
-    characterId: e.target.dataset.id || undefined,
+    content: commentText,
+    characterId: id,
   };
-  comments.push(newComment);
-  const commentsList = document.getElementById(`comment-card-${returnedChar.id}`);
+  addCommentToDatabase(newComment);
+  renderComment(commentsContainer, newComment);
 }
+
+
 /**
  * ? Darrian created this function instead of replacing the content of the pokeContainer
  * */
+// Hide all pokemon for show page
 function hideAllPokemon() {
-  const allPokemon = document.querySelectorAll(".poke-card");
-  allPokemon.forEach(poke => poke.setAttribute("hidden", true));
+  const pokeCardEls = document.querySelectorAll(".poke-card");
+  pokeCardEls.forEach(pokeCardEl => pokeCardEl.classList.add("hide-card"));
+}
+// Show all pokemon
+function showAllPokemon() {
+  const pokeCardEls = document.querySelectorAll(".poke-card");
+  pokeCardEls.forEach(pokeCardEl => pokeCardEl.classList.remove("hide-card"));
 }
 
 // reminder: when we invoke our commentsForm method as our argument above, we are passing the RETURN VALUE of the function to our replaceChildren method. The return value is the comments form
 
 // CREATE COMMENT FORM FOR SHOW PAGE
-function commentsForm(character) {
+function commentsForm(pokemon) {
   const form = document.createElement("form");
   form.id = "comment-form";
-  if (character) {
-    form.dataset.id = character.id;
-  }
+  form.dataset.id = pokemon.id;
 
   // attach an event listener to the #comment-form here
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const textInput = e.target.children['comment-input'].value;
     e.target.children['comment-input'].value = "";
-    addComment(textInput, character.id);
+    addComment(textInput, pokemon.id);
   });
 
   const commentInput = document.createElement("input");
@@ -158,73 +175,117 @@ function renderComment(ul, comment) {
 
 
 // INITIALIZE
+// get pokemon from database by id
+function getPokemonById(id) {
+  return fetch(`http://localhost:3000/characters/${id}`)
+    .then(resp => resp.json())
+    .then(pokemon => {
+      return pokemon;
+    });
+}
+
 // get all pokemon from database
-function getAllPokemon() {
+function fetchAllPokemon() {
   return fetch("http://localhost:3000/characters")
     .then(resp => resp.json());
 }
+
+function removeCard(id) {
+  const cards = document.querySelectorAll(`.poke-id-${id}`);
+  cards.forEach(card => card.remove());
+}
+
 // render all pokemon
 function renderAllPokemon() {
-  getAllPokemon().then(pokemonArray => {
-    renderPokemon(pokemonArray);
+  fetchAllPokemon().then(pokemonArray => {
+    console.log(pokemonArray);
+    pokeContainer.append(...renderPokemon(pokemonArray));
   });
 }
 
-// INVOKE INITIALIZE
-renderAllPokemon(getAllPokemon());
+// render one pokemon
+function renderPokemon(pokemonArray) {
+  pokeCardEls = pokemonArray.map(pokemon => createPokeCardEl(pokemon));
+  console.log(pokeCardEls);
+  return pokeCardEls;
+}
 
+function createPokeCardEl(pokemon) {
+  const pokeId = pokemon.id;
+  const pokeImg = pokemon.img;
+  const pokeName = pokemon.name;
 
-function renderPokemon(characterArray) {
-  characterArray.forEach(character => {
-    const pokeCard = document.createElement("div");
-    pokeCard.id = `poke-${character.id}`;
-    pokeCard.className = "poke-card";
+  const pokeCardEl = document.createElement("div");
+  pokeCardEl.classList.add(`poke-id-${pokeId}`);
+  pokeCardEl.classList.add("poke-card");
 
-    pokeCard.addEventListener("click", () => showCharacter(character));
-
-    if (character.img) {
-      const pokeImg = document.createElement("img");
-      pokeImg.src = character.img;
-      pokeImg.alt = `picture of ${character.name}`;
-      pokeCard.append(pokeImg);
-    }
-
-    const pokeName = document.createElement("h3");
-    pokeName.innerText = character.name;
-
-    const pokeLikes = document.createElement("h3");
-    pokeLikes.innerText = "Likes: ";
-
-    const likesNum = document.createElement("h5");
-    likesNum.className = "likes-num";
-    likesNum.textContent = character.likes;
-
-    const likeBtn = document.createElement("button");
-    likeBtn.className = "like-btn";
-    likeBtn.textContent = "♥";
-
-    likeBtn.addEventListener("click", addLikes);
-
-    function addLikes(e) {
-      e.stopPropagation();
-      character.likes += 1;
-      likesNum.textContent = character.likes;
-    }
-
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.className = "delete-btn";
-    deleteBtn.textContent = "Delete";
-
-    deleteBtn.addEventListener("click", (e) => {
-      e.stopPropagation();
-      pokeCard.remove();
-    });
-
-    pokeCard.append(pokeName, pokeLikes, likesNum, likeBtn, deleteBtn);
-    pokeContainer.append(pokeCard);
-
-    // returning our pokeCard so we can use the return value of the render function in our pokeCard div event listener
-    return pokeCard;
+  pokeCardEl.addEventListener("click", () => {
+    pokemonShowPage(pokemon);
   });
+
+  if (pokeImg) {
+    const pokeImgEl = document.createElement("img");
+    pokeImgEl.src = pokeImg;
+    pokeImgEl.alt = `picture of ${pokeName}`;
+    pokeCardEl.appendChild(pokeImgEl);
+  }
+
+  const pokeNameEl = document.createElement("h3");
+  pokeName.innerText = pokeName;
+  pokeCardEl.appendChild(pokeNameEl);
+
+  const deleteBtnEl = createDeleteBtn(pokemon, pokeId);
+  pokeCardEl.appendChild(deleteBtnEl);
+
+  const likeBtnEl = createLikeBtn(pokemon, pokeId);
+  pokeCardEl.appendChild(likeBtnEl);
+
+  // ! No longer appends, append upon return!
+  // pokeContainer.append(pokeCardEl);
+
+  // returning our pokeCardEl so we can use the return value of the render function in our pokeCardEl div event listener
+
+  return pokeCardEl;
+}
+
+function createDeleteBtn(pokemon, pokeId) {
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete-btn";
+  deleteBtn.textContent = "Delete";
+
+  deleteBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    remove(pokeId);
+  });
+
+  return deleteBtn;
+}
+
+function createLikeBtn(pokemon, pokeId) {
+  let likeCount = pokemon.likes;
+
+  const pokeLikes = document.createElement("h3");
+  pokeLikes.innerText = "Likes: ";
+
+  const likesNum = document.createElement("h5");
+  likesNum.className = "likes-num";
+  likesNum.textContent = likeCount;
+
+  const likeBtn = document.createElement("button");
+  likeBtn.className = "like-btn";
+  likeBtn.textContent = "♥";
+
+  likeBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    likeCount++;
+    pokemonDB.updatePokemon(pokeId, { "likes": likeCount });
+    likesNum.innerText = likeCount;
+  });
+
+  const likesContainer = document.createElement("div");
+  likesContainer.classList.add("likes-container");
+  likesContainer.append(pokeLikes, likesNum, likeBtn);
+
+  return likesContainer;
 }
